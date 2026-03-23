0d3c4615362a13f716c7e8ab53a3a5c2a576e7f3
leonardonumatchipepes-droid <leonardonumatchipepes@gmail.com>
2026-03-23 15:34:08 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "tuye-cache-v1";
+const urlsToCache = [
+  "./index.html",
+  "./assets/logo.png",
+  "./assets/logo-192.png",
+  "./assets/logo-512.png",
+  "./manifest.json",
+  "./style.css"
+];
+
+// Instalando Service Worker e cache inicial
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Ativando Service Worker
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interceptando requisições
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
